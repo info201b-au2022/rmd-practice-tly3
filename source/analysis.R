f04219ec3918678d4d3d9278120999c9dd703a58
thanhly3 <thanhly3@cs.washington.edu>
2022-11-02 20:17:49 -0700

    created html file from rmd file

--- source/analysis.R
+++ source/analysis.R
@@ -21,8 +21,9 @@ get_total_deaths <- function() {
   total_deaths <- national %>%
     filter(deaths == max(deaths)) %>%
     pull(deaths)
-  return(total_deaths)
+  return(prettyNum(total_deaths,big.mark=",",scientific=FALSE))
 }
+
 
 # Run the following code to create a plot of cumulative cases over time
 # (we'll explain the ggplot2 syntax next week)
